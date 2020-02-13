450f97477afef249dbf9b3bc77abd22f50329b46
Tyler Alsop <tylerjalsop@gmail.com>
2020-02-12 18:23:08 -0700

    Setup the TodoList function to return the uncompleted task cards while passing the appropriate props into <TaskCard />, and a button to clear the completed tasks.

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -2,3 +2,23 @@ import React from 'react';
 
 import TaskCard from "./TaskCard";
 
+const TodoList = props => {
+
+    return (
+        <div className="todo-list">
+            {props.todo.map(task => (
+                <TaskCard 
+                    key={task.id} 
+                    task={task} 
+                    toggleTaskCompleted={props.toggleTaskCompleted} />
+            ))}
+
+            <button className="clear-completed-button" onClick={props.clearCompleted}>
+                Clear Completed Tasks
+            </button>
+
+        </div>
+    );
+};
+
+export default TodoList;
